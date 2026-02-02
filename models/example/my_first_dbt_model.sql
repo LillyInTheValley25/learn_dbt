4b9e0b637220dc3e5adba7d624358f477a0d1aac
Nina Dusk <nina.dusk.2025@gmail.com>
2026-02-02 15:41:46 +0000

    snapshots

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -11,17 +11,12 @@
 
 with source_data as (
 
-    select 1 as id
+    select 1 as id, 'NY' as state, '2020-02-01 00:01:00.000'::timestamp as updated_at
     union all
-    select null as id
-
+    select null as id, 'CT' as state, '2020-01-01 00:00:00.000'::timestamp as updated_at
+    union all
+    select 3 as id, 'VT' as state, '2020-01-01 00:00:00.000'::timestamp as updated_at
 )
 
 select *
 from source_data
-
-/*
-    Uncomment the line below to remove records with null `id` values
-*/
-
--- where id is not null
